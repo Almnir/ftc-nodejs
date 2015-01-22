fb08b421be79aeea3b267558d29c40cd52c0a992
Almnir <almnir.mail@gmail.com>
2015-01-22 16:21:20 +0400

    Added SQLite, rendering posts. Doesn't work properly.

--- app.js
+++ app.js
@@ -1,16 +1,17 @@
-
 /**
  * Module dependencies.
  */
 
-var express = require('express')
-  , routes = require('./routes')
-  , http = require('http')
-  , path = require('path')
-  , bodyParser = require('body-parser')
-  , favicon = require('serve-favicon')
-  , logger = require('morgan')
-  , methodOverride = require('method-override');
+var express = require('express'),
+    routes = require('./routes'),
+    http = require('http'),
+    path = require('path'),
+    bodyParser = require('body-parser'),
+    favicon = require('serve-favicon'),
+    logger = require('morgan'),
+    methodOverride = require('method-override'),
+    sqlite3 = require('sqlite3').verbose(),
+    db = new sqlite3.Database('ftc.db');
 
 var app = express();
 
@@ -19,17 +20,66 @@ app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
 app.use(favicon(__dirname + '/public/images/ftc_logo_fav.png'));
 app.use(logger('dev'));
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+// app.use(multer()); // for parsing multipart/form-data
 app.use(methodOverride('_method'));
 app.use(require('stylus').middleware(__dirname + '/public'));
 app.use(express.static(path.join(__dirname, 'public')));
 
 if (app.get('env') == 'development') {
-	app.locals.pretty = true;
+    app.locals.pretty = true;
 }
 
-app.get('/', routes.index);
+// -----------------------DB initialization---------------------------
 
-http.createServer(app).listen(app.get('port'), function(){
-  console.log("Express server listening on port " + app.get('port'));
+db.get("SELECT name FROM sqlite_master WHERE type='table' AND name='posts'",
+    function(err, rows) {
+        if (err !== null) {
+            console.log(err);
+        } else if (rows === undefined) {
+            db.run('CREATE TABLE "posts" ' +
+                '("id" INTEGER PRIMARY KEY AUTOINCREMENT, ' +
+                '"title" VARCHAR(255), ' +
+                '"post" VARCHAR(255), ' +
+                'url VARCHAR(255))', function(err) {
+                    if (err !== null) {
+                        console.log(err);
+                    } else {
+                        console.log("SQL Table 'posts' initialized.");
+                    }
+                });
+        } else {
+            console.log("SQL Table 'posts' already initialized.");
+        }
+    });
+
+// === routing ===
+app.get('/', function(req, res) {
+    db.all('SELECT * FROM posts ORDER BY title', function(err, row) {
+        if (err !== null) {
+            res.send(500, "An error has occurred -- " + err);
+        } else {
+            console.log(row);
+            res.render('index.jade', {
+                posts: row
+            }, function(err, html) {
+                res.send(200, html);
+            });
+        }
+    });
+};);
+
+app.get('/about', function(req, res) {
+    res.send('Hello World!');
+});
+
+app.get('/files', function(req, res) {
+
+});
+
+// **************server start****************
+http.createServer(app).listen(app.get('port'), function() {
+    console.log("Express server listening on port " + app.get('port'));
 });
--- routes/index.js
+++ routes/index.js
@@ -1,3 +1,14 @@
-exports.index = function(req, res){
-  res.render('index');
+exports.index = function(req, res) {
+    db.all('SELECT * FROM posts ORDER BY title', function(err, row) {
+        if (err !== null) {
+            res.send(500, "An error has occurred -- " + err);
+        } else {
+            console.log(row);
+            res.render('index.jade', {
+                posts: row
+            }, function(err, html) {
+                res.send(200, html);
+            });
+        }
+    });
 };
